d8d9a27c930c9c1b5e7d11a31f60195224e8b4fd
jregoma <juan.regomartinez@gmail.com>
2025-11-18 09:55:17 +0100

    programa media

---
+++ media.py
@@ -0,0 +1,7 @@
+import numpy as np
+#Crear un array e calcular a súa media
+print("Hola mundo")
+array=np.array([1,2,3,4,5])
+media=np.mean(array) #Cálculo da media
+print(f"Media do array: {media}")
+print("Adiós mundo")
