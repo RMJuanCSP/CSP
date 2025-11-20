28879341d6a96b050a98175db17516b119c65442
Juan <juan.regomartinez@gmail.com>
2025-11-20 19:41:13 +0100

    Update media.py

--- media.py
+++ media.py
@@ -5,4 +5,4 @@ array=np.array([1,2,3,4,5])
 media=np.mean(array) #Cálculo da media
 print(f"Media do array: {media}")
 print("Adiós mundo")
-print("Esta é unha proba de do arquivo e subida a Github")
+print("Esta é unha proba de modificación do arquivo e subida a Github")
